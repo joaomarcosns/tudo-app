b70d8baf43acf259ebac1c793e7fbe0207f38d78
João Marcos Neves da Silva <joaomarcosnina@gmail.com>
2023-10-11 16:28:08 -0300

    ➕ firebase

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,17 +1,45 @@
 import React from "react";
 import { View, Text, Button } from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import firestore from "@react-native-firebase/firestore";
 
 export function Home() {
   const navigation = useNavigation();
+  const ref = firestore().collection("category");
+
   function goToTask() {
     navigation.navigate("Task");
+  }
+
+  async function addTodo() {
+    try {
+      await ref.add({
+        title: "teste",
+        complete: false,
+        task: [
+          {
+            title: "teste task 1",
+            complete: false,
+          },
+          {
+            title: "teste task 2",
+            complete: false,
+          },
+        ],
+      });
+    } catch (error) {
+      console.log('====================================');
+      console.log(error);
+      console.log('====================================');
+    }
   }
   return (
     <View>
       <Text>Home</Text>
 
       <Button title="Ir para outra tela" onPress={goToTask} />
+      <Text>Salvar Firebase</Text>
+      <Button title="Salvar" onPress={addTodo} />
     </View>
   );
 }
